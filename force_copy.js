aad1636ba81da67a5bd9bd46f80d06aa5bdb36b4
Winifred <debpuurwinifred@gmail.com>
2025-12-11 09:18:57 +0000

    chore: Update App Icon and Favicon to new version

--- force_copy.js
+++ force_copy.js
@@ -2,7 +2,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const source = "C:\\Users\\Cornelius Debpuur\\.gemini\\antigravity\\brain\\97a95dd7-1305-4d31-b6eb-1f5066679063\\uploaded_image_1765442102760.png";
+const source = "C:\\Users\\Cornelius Debpuur\\.gemini\\antigravity\\brain\\97a95dd7-1305-4d31-b6eb-1f5066679063\\uploaded_image_1765444666066.png";
 const dest = "c:\\Users\\Cornelius Debpuur\\Desktop\\Web Projects\\Branded By Winni\\public\\logo-icon.png";
 
 console.log(`Copying from ${source} to ${dest}`);
