bc3cdad35b84656f3f5653289188776cf4571860
Winifred <debpuurwinifred@gmail.com>
2026-01-11 08:20:32 +0000

    Self-host fonts: Removed Google Fonts CDN and added @fontsource packages

--- index.tsx
+++ index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@fontsource/inter/300.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/playfair-display/400.css';
+import '@fontsource/playfair-display/500.css';
+import '@fontsource/playfair-display/600.css';
+import '@fontsource/playfair-display/400-italic.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
